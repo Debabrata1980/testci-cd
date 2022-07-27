d43329012e907909f2ad78b634b8ff41d98b977e
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2022-07-27 17:18:51 +0530

    test

---
+++ testrun.sh
@@ -0,0 +1,37 @@
+i#!/bin/sh
+
+# First argument is the folder containing the python code (default to current folder).
+python_folder="."
+if [ "$1" != "" ]; then
+  python_folder="$1"
+fi
+
+if [ -f "${python_folder}/requirements.txt" ]; then
+  pip3 install -q -r ${python_folder}/requirements.txt
+fi
+
+echo -e "\nPython files:"
+ls ${python_folder}/*.py
+
+# Run python linting. Ignoring E501 (line too long)
+echo -e "\nLinting python files..."
+flake8 --ignore E501 ${python_folder}
+resp=$?
+# Exit if there are any errors 
+if [ ${resp} -ne 0 ]; then
+  exit 1
+fi
+
+# Execute unit tests
+echo -e "\nExecuting unit tests..."
+found_errors=0
+for python_file in ${python_folder}/*.py; do
+  python -m unittest ${python_file}
+  if [ $? -ne 0 ]; then
+    found_errors=1
+  fi
+done
+# Exit if there are any errors 
+if [ ${found_errors} -ne 0 ]; then
+  exit 1
+fi

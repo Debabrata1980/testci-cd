8368b3c26693bd70831589f7abebe98de01a8de5
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2022-08-04 17:26:44 +0530

    test

--- tests/db_conn_test.py
+++ tests/db_conn_test.py
@@ -7,7 +7,7 @@ import unittest
 # from botocore.exceptions import ClientError
 # import botocore.session
 # from aws_secretsmanager_caching import SecretCache, SecretCacheConfig
-from db_conn import *
+import db_conn 
 from unittest.mock import patch
 
 
@@ -35,7 +35,7 @@ class TestConnection(unittest.TestCase):
     @patch('db_conn.db_conn')
     def test_db_conn(self, mock_db):
       mock_db.return_value.query_all_data.return_value = 'result data'
-      result = get_data()
+      result = db_conn.get_data()
       self.assertEqual(result, 'result data')
       self.assertEqual(mock_db.call_count, '1')
       self.assertEqual(mock_db.query_all_data.call_count, 1)

16cef9861a6ea13b9f260ab904fd37c3c87e2300
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2022-08-04 16:54:35 +0530

    test

--- tests/db_conn.py
+++ tests/db_conn.py
@@ -10,8 +10,9 @@ RDS = "SecretDBName1"
 sm_client = boto3.client('secretsmanager', region_name='us-west-2')
 conn = None
 
+
 def db_conn():
-    try :
+    try:
         pg_credential = json.loads(sm_client.get_secret_value(SecretId=RDS)['SecretString'])
         conn = psycopg2.connect(host=pg_credential.get('host'),
                                 port=pg_credential.get('port'),
@@ -20,8 +21,9 @@ def db_conn():
                                 database=pg_credential.get('dbname'))
 
         return conn
-    except ClientError as e:
-            print(e)
+    except ClientError as e:            
+        print(e)
+        
         
 def get_date():
         dbconn = db_conn()
@@ -32,6 +34,8 @@ def get_date():
         print('print the records returned after database connection', a)
         return a
 
+
 if __name__ == "__main__":
 
     db_conn()
+    
--- tests/db_conn_test.py
+++ tests/db_conn_test.py
@@ -48,7 +48,7 @@ class TestConnection(unittest.TestCase):
       result = get_data()
     
       self.assertEqual(result, 'result data')
-      self.assertEqual(mock_db.call_count, 1')
+      self.assertEqual(mock_db.call_count, '1')
       self.assertEqual(mock_db.query_all_data.call_count, 1)
 
 if __name__ == '__main__':

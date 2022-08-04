9c13fc987fbe25af1f380e13151111adeb2e1ac0
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2022-08-04 15:01:18 +0530

    tests

--- tests/testpy.py
+++ tests/testpy.py
@@ -12,10 +12,10 @@ class TestStringMethods(unittest.TestCase):
 
     def test_split(self):
         s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
+        self.assertEqual(s.split(), ['hello', 'abcd'])
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
-            s.split()
+            s.split(2)
 
 
 if __name__ == '__main__':

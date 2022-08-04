c568f35f84ab610d901281f9a9b1555a99f76323
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2022-08-04 13:35:17 +0530

    test

--- testrun.sh
+++ testrun.sh
@@ -28,8 +28,12 @@ found_errors=0
 for python_file in ${python_folder}/*.py; do
   echo "File Name "  ${python_file}
   pytest  ${python_file}
+  
+  echo "found error "  ${found_error} 
   if [ $? -ne 0 ]; then
-    found_errors=1
+    found_errors+=1
+	echo "found error "  ${found_error}   
+	
   fi
 done
 # Exit if there are any errors 

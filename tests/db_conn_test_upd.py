f29b0b7ee76ce8fea68bd2e02349f7936c5e3252
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2022-08-08 13:16:05 +0530

    test

--- tests/db_conn_test_upd.py
+++ tests/db_conn_test_upd.py
@@ -13,49 +13,51 @@ import os
 # from unittest.mock import patch
 from moto import mock_secretsmanager,mock_rds
 import sure # noqa # pylint: disable=unused-import
+#import src.db_conn
 
 
-@mock_secretsmanager
-def test_get_secret_value():
-    os.environ['RDS'] = 'stellarbi/rds'
-    conn = boto3.client("secretsmanager", region_name="us-west-2")
+class MyUnitTest(unittest.TestCase):
+    @mock_secretsmanager
+    def test_get_secret_value(self):
+        os.environ['RDS'] = 'stellarbi/rds'
+        conn = boto3.client("secretsmanager", region_name="us-west-2")
 
-    conn.create_secret(Name=os.environ.get('RDS'), SecretString="foosecret")
-    result = conn.get_secret_value(SecretId=os.environ.get('RDS'))
-    assert result["SecretString"] == "foosecret"
+        conn.create_secret(Name=os.environ.get('RDS'), SecretString="foosecret")
+        result = conn.get_secret_value(SecretId=os.environ.get('RDS'))
+        assert result["SecretString"] == "foosecret"
 
 
-@mock_rds
-def test_start_database():
-    from src.db_conn import pg_credential
-    conn = boto3.client("rds", region_name="us-west-2")
-    database = conn.create_db_instance(
-        DBInstanceIdentifier="db-master-1",
-        AllocatedStorage=10,
-        Engine="postgres",
-        DBName=pg_credential.get('dbname'),
-        DBInstanceClass="db.m1.small",
-        LicenseModel="license-included",
-        MasterUsername=pg_credential.get['username'],
-        MasterUserPassword=pg_credential.get('password'),
-        Port=pg_credential.get('port'),
-        DBSecurityGroups=["my_sg"],
-    )
-    mydb = conn.describe_db_instances(
-        DBInstanceIdentifier=database["DBInstance"]["DBInstanceIdentifier"]
-    )["DBInstances"][0]
-    print(mydb)
-    mydb["DBInstanceStatus"].should.equal("available")
+    @mock_rds
+    def test_start_database(self):
+        from src.db_conn import pg_credential
+        conn = boto3.client("rds", region_name="us-west-2")
+        database = conn.create_db_instance(
+            DBInstanceIdentifier="db-master-1",
+            AllocatedStorage=10,
+            Engine="postgres",
+            DBName=pg_credential.get('dbname'),
+            DBInstanceClass="db.m1.small",
+            LicenseModel="license-included",
+            MasterUsername=pg_credential.get['username'],
+            MasterUserPassword=pg_credential.get('password'),
+            Port=pg_credential.get('port'),
+            DBSecurityGroups=["my_sg"],
+        )
+        mydb = conn.describe_db_instances(
+            DBInstanceIdentifier=database["DBInstance"]["DBInstanceIdentifier"]
+        )["DBInstances"][0]
+        print(mydb)
+        mydb["DBInstanceStatus"].should.equal("available")
 
-#    connection = db_conn()   # How I will check to connect to the database I have created by this function of db_conn
+    #    connection = db_conn()   # How I will check to connect to the database I have created by this function of db_conn
 
-    response = conn.stop_db_instance(
-        DBInstanceIdentifier=mydb["DBInstanceIdentifier"],
-        DBSnapshotIdentifier="rocky4570-rds-snap",
-    )
-    response["DBInstance"]["DBInstanceStatus"].should.equal("stopped")
+        response = conn.stop_db_instance(
+            DBInstanceIdentifier=mydb["DBInstanceIdentifier"],
+            DBSnapshotIdentifier="rocky4570-rds-snap",
+        )
+        response["DBInstance"]["DBInstanceStatus"].should.equal("stopped")
 
-    conn.delete_db_instance(
-        DBInstanceIdentifier="db-master-1",
-        FinalDBSnapshotIdentifier="primary-1-snapshot",
-    )
+        conn.delete_db_instance(
+            DBInstanceIdentifier="db-master-1",
+            FinalDBSnapshotIdentifier="primary-1-snapshot",
+        )

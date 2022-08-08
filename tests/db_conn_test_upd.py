5ac814d4986f8c53becb32e902ba11e89179cd5f
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2022-08-08 15:16:41 +0530

    test

--- tests/db_conn_test_upd.py
+++ tests/db_conn_test_upd.py
@@ -101,7 +101,7 @@ class MyUnitTest(unittest.TestCase):
                         REFERENCES parts (part_id)
                         ON UPDATE CASCADE ON DELETE CASCADE
               )
-             """
+             """)
          connection = psycopg2.connect(host=pg_credential.get('host'),
                             port=pg_credential.get('port'),
                             user=pg_credential.get('username'),

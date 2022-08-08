5eae5ff004c1b557592e0962c9bbcbc6f87ca46f
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2022-08-08 14:53:46 +0530

    test

--- tests/db_conn_test_upd.py
+++ tests/db_conn_test_upd.py
@@ -3,7 +3,7 @@ import boto3
 import os
 import unittest
 # import json
-# import psycopg2
+import psycopg2
 # import db_conn
 # import botocore
 # from botocore.exceptions import ClientError
@@ -29,20 +29,34 @@ class MyUnitTest(unittest.TestCase):
 
     @mock_rds
     def test_start_database(self):
-        from src.db_conn import pg_credential
-        conn = boto3.client("rds", region_name="us-west-2")
-        database = conn.create_db_instance(
+#        from src.db_conn import pg_credential
+         conn = boto3.client("rds", region_name="us-west-2")
+#        database = conn.create_db_instance(
+#            DBInstanceIdentifier="db-master-1",
+#            AllocatedStorage=10,
+#            Engine="postgres",
+#            DBName=pg_credential.get('dbname'),
+#            DBInstanceClass="db.m1.small",
+#            LicenseModel="license-included",
+#            MasterUsername=pg_credential.get['username'],
+#            MasterUserPassword=pg_credential.get('password'),
+#            Port=pg_credential.get('port'),
+#            DBSecurityGroups=["my_sg"],
+#        )
+
+         database = conn.create_db_instance(
             DBInstanceIdentifier="db-master-1",
             AllocatedStorage=10,
             Engine="postgres",
-            DBName=pg_credential.get('dbname'),
+            DBName="test",
             DBInstanceClass="db.m1.small",
             LicenseModel="license-included",
-            MasterUsername=pg_credential.get['username'],
-            MasterUserPassword=pg_credential.get('password'),
-            Port=pg_credential.get('port'),
+            MasterUsername="test-db-user",
+            MasterUserPassword="test1234",
+            Port="5432",
             DBSecurityGroups=["my_sg"],
         )
+
         mydb = conn.describe_db_instances(
             DBInstanceIdentifier=database["DBInstance"]["DBInstanceIdentifier"]
         )["DBInstances"][0]
@@ -50,8 +64,59 @@ class MyUnitTest(unittest.TestCase):
         mydb["DBInstanceStatus"].should.equal("available")
 
     #    connection = db_conn()   # How I will check to connect to the database I have created by this function of db_conn
+    
+        """ create tables in the PostgreSQL database"""
+        commands = (
+            """
+            CREATE TABLE vendors (
+                vendor_id SERIAL PRIMARY KEY,
+                vendor_name VARCHAR(255) NOT NULL
+            )
+            """,
+            """ CREATE TABLE parts (
+                    part_id SERIAL PRIMARY KEY,
+                    part_name VARCHAR(255) NOT NULL
+                    )
+            """,
+            """
+            CREATE TABLE part_drawings (
+                    part_id INTEGER PRIMARY KEY,
+                    file_extension VARCHAR(5) NOT NULL,
+                    drawing_data BYTEA NOT NULL,
+                    FOREIGN KEY (part_id)
+                    REFERENCES parts (part_id)
+                    ON UPDATE CASCADE ON DELETE CASCADE
+            )
+            """,
+            """
+            CREATE TABLE vendor_parts (
+                    vendor_id INTEGER NOT NULL,
+                    part_id INTEGER NOT NULL,
+                    PRIMARY KEY (vendor_id , part_id),
+                    FOREIGN KEY (vendor_id)
+                        REFERENCES vendors (vendor_id)
+                        ON UPDATE CASCADE ON DELETE CASCADE,
+                    FOREIGN KEY (part_id)
+                        REFERENCES parts (part_id)
+                        ON UPDATE CASCADE ON DELETE CASCADE
+            )
+            """
+        connection = psycopg2.connect(host=pg_credential.get('host'),
+                            port=pg_credential.get('port'),
+                            user=pg_credential.get('username'),
+                            password=pg_credential.get('password'),
+                            database=pg_credential.get('dbname')
 
-        response = conn.stop_db_instance(
+        cur = connection.cursor()
+        # create table one by one
+        for command in commands:
+            cur.execute(command)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        connection.commit()
+
+       response = conn.stop_db_instance(
             DBInstanceIdentifier=mydb["DBInstanceIdentifier"],
             DBSnapshotIdentifier="rocky4570-rds-snap",
         )

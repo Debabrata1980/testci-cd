7d0cd3cc4963d8cdd9388678f133c8c3ecc9e53e
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2022-08-08 23:46:52 +0530

    test

--- testrun.sh
+++ testrun.sh
@@ -18,9 +18,9 @@ echo -e "\nLinting python files..."
 #flake8 --ignore=E1,E23,W503,E226,W391,E501  ${python_folder}
 #resp=$?
 # Exit if there are any errors 
-if [ ${resp} -ne 0 ]; then
-  exit 1
-fi
+#if [ ${resp} -ne 0 ]; then
+#  exit 1
+#fi
 
 # Execute unit tests
 echo -e "\nExecuting unit tests..."
@@ -31,9 +31,9 @@ found_errors=0
 #pytest  ${python_folder}/secret_test.py
 
 
-for python_file in ${python_folder}/*.py; do
-  echo "File Name "  ${python_file}
-  cd ./
+#for python_file in ${python_folder}/*.py; do
+#  echo "File Name "  ${python_file}
+  cd ./tests
   pytest  -s #${python_file} --All files will be tested
 
   
--- tests/s3_test.py
+++ tests/s3_test.py
@@ -1,28 +1,33 @@
 import boto3
 from moto import mock_s3
 from src.mymodule import MyModel
+from src import *
 
 
 class MyUnitTest(unittest.TestCase):
 
+        BUCKET_NAME = "mybucket"
+        FILE_NAME = "red.jpg"
+        FILE_LOCATION = FILE_NAME
+        
         @mock_s3
         def test_my_model_save(self):
             conn = boto3.resource('s3', region_name='us-east-1')
             # We need to create the bucket since this is all in Moto's 'virtual' AWS account
-            conn.create_bucket(Bucket='mybucket')
+            conn.create_bucket(Bucket=BUCKET_NAME)
             model_instance = MyModel('steve', 'is awesome')
             model_instance.save()
-            body = conn.Object('mybucket', 'steve').get()['Body'].read().decode("utf-8")
+            body = conn.Object(BUCKET_NAME, 'steve').get()['Body'].read().decode("utf-8")
             assert body == 'is awesome'
 
-        """
+        
         @mock_s3
         def test_archive_s3(self):
             from src.schema_reader import _download_file
             from rollback import CErrorTypes, send_record_to_s3, archive
         #    import json
             conn = boto3.resource('s3', region_name='us-east-1')
-            conn.create_bucket(Bucket='mybucket')
+            conn.create_bucket(Bucket=BUCKET_NAME)
             data = {
               "type": "com.hp.id.data.internal.profile_create.v1",
               "id": "sdfgregfdgh54hyth56hrth56hrhtryj",
@@ -38,4 +43,13 @@ class MyUnitTest(unittest.TestCase):
               }
             }
             client = boto3.client('s3', region_name='us-east-1')
-            """
+            with open(FILE_LOCATION, 'rb') as data:
+                client.upload_fileobj(data, BUCKET_NAME, FILE_NAME)
+            
+            ###  Print
+            
+            resp = client.get_object(Bucket=BUCKET_NAME, Key=FILE_NAME)
+            content_length = resp["ResponseMetadata"]["HTTPHeaders"]["content-length"]
+            print("Content-Length: {}".format(content_length))
+            
+            

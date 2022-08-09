f6b9455e77857fd2de631dad99440d47de90b8b1
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2022-08-09 17:56:10 +0530

    test

--- testrun.sh
+++ testrun.sh
@@ -31,10 +31,10 @@ found_errors=0
 #pytest  ${python_folder}/secret_test.py
 
 
-#for python_file in ${python_folder}/*.py; do
-#  echo "File Name "  ${python_file}
-  cd ./tests
-  python -m pytest -s ./tests #${python_file} --All files will be tested
+for python_file in ${python_folder}/*.py; do
+  echo "File Name "  ${python_file}
+
+  python -m pytest -s ${python_file} --All files will be tested
 
   
   echo "found error "  ${found_errors} 
@@ -43,7 +43,7 @@ found_errors=0
 	echo "found error "  ${found_errors}   
 	
   fi
-#done
+done
 # Exit if there are any errors 
 if [ ${found_errors} -ne 0 ]; then
 	echo "found_error :" ${found_errors}

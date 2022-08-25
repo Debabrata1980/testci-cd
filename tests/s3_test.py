fc3ba66eea432a184bf528cae96c00f844be31ed
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2022-08-25 11:28:10 +0530

    test

--- tests/s3_test.py
+++ tests/s3_test.py
@@ -53,9 +53,9 @@ class MyUnitTest(unittest.TestCase):
         def test_download_from_s3(self):
             read_file = Schema(self.FILE_NAME)
             conn = boto3.resource('s3', region_name='us-east-1')
-            conn.create_bucket(Bucket=self.BUCKET_NAME)
+            buck=conn.create_bucket(Bucket=self.BUCKET_NAME)
             #client = boto3.client('s3', region_name='us-east-1')
-            conn.put_object(Bucket=self.BUCKET_NAME, Key= self.FILE_NAME, Body='')
+            buck.put_object(Bucket=self.BUCKET_NAME, Key= self.FILE_NAME, Body='')
             resp1 = read_file._download_file(self.BUCKET_NAME,self.FILE_LOCATION_DOWNLOAD)
             print(resp1)
             assert os.path.isfile(self.FILE_NAME)

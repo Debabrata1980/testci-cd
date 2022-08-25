a64e1db477b8dee05a505df35427f35f3fa2c935
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2022-08-25 18:51:37 +0530

    test

--- tests/s3_test.py
+++ tests/s3_test.py
@@ -47,8 +47,8 @@ class MyUnitTest(unittest.TestCase):
             content_length = resp["ResponseMetadata"]["HTTPHeaders"]["content-length"]
             respone = resp["ResponseMetadata"]["HTTPStatusCode"]
             print("Content-Length: {}".format(content_length))
-            assert content_length == '36971'
-            assert respone == '200' 
+            assert content_length == 36971
+            assert respone == 200 
  
                # client.upload_fileobj(data, self.BUCKET_NAME, self.FILE_NAME)
                # resp = client.get_object(Bucket=self.BUCKET_NAME, Key=self.FILE_NAME)

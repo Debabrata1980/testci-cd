6b342e02ffa91d568505b75ba429f403eb6fde9b
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2022-09-05 22:32:10 +0530

    test

---
+++ tests/db_test_upd.py
@@ -0,0 +1,22 @@
+import unittest
+import os
+import sqlalchemy
+
+#import myapp
+
+class TestDB(unittest.TestCase):
+	DB_TEST_URL = ""
+    def setUp(self):
+       url = os.getenv(self.DB_TEST_URL)
+       if not url:
+           self.skipTest("No database URL set")
+       self.engine = sqlalchemy.create_engine(url)
+
+    def test_foobar(self):
+	    pass
+        #self.assertTrue(myapp.store_integer(self.engine, 42))
+
+if __name__ == '__main__':
+    if len(sys.argv) > 0:
+        TestDB.DB_TEST_URL = sys.argv.pop()
+    unittest.main()

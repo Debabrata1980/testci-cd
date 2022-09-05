b98110732a96b6aa2b3c1202f2e05afd0239d377
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2022-09-05 19:01:36 +0530

    Test

--- tests/db_test.py
+++ tests/db_test.py
@@ -19,8 +19,8 @@ import pgmock.exceptions
 
 #db_conn = sqlalchemy.create_engine('postgresql://localhost:5432/local-db')
 
-print(process.env.POSTGRES_HOST)
-print(process.env.POSTGRES_PORT)
+print(POSTGRES_HOST)
+print(POSTGRES_PORT)
 
 DB_URL = 'postgresql://POSTGRES_HOST:POSTGRES_PORT/postgres'
 

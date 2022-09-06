dfe418fe12c784e71b1c73306390cbf69d83781a
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2022-09-06 12:45:54 +0530

    test

--- tests/db_test_upd.py
+++ tests/db_test_upd.py
@@ -13,7 +13,7 @@ class TestDB(unittest.TestCase):
        if not url:
            self.skipTest("No database URL set")
        self.engine = sqlalchemy.create_engine(url)
-#       self.connection = self.engine.connect()
+       self.connection = self.engine.connect()
 #       self.connection = self.engine.begin()
 #       self.connection.execute("CREATE DATABASE testdb")
 
@@ -21,8 +21,15 @@ class TestDB(unittest.TestCase):
        
        
    def test_foobar(self):
+   
+        trans = self.connection.begin()
+        self.connection.execute('''CREATE TABLE accounts (age VARCHAR ( 50 ),name VARCHAR ( 50 ));''')
+        self.connection.execute('''INSERT INTO accounts (age,"name") values('22','deb')''')
+        trans.commit()
+
+        '''
         with self.engine.connect() as connection:
-            connection.execute(text("CREATE Table test1(age VARCHAR ( 50 ),name VARCHAR ( 50 ))"))
+            connection.execute("CREATE Table test1(age VARCHAR ( 50 ),name VARCHAR ( 50 ))")
             with connection.begin():                
 #                r1 = connection.execute(test1.select())
                 connection.execute(test1.insert(), {"age": '7', "name": "this is some data"})       
@@ -35,7 +42,7 @@ class TestDB(unittest.TestCase):
  #      result = list(self.connection.execute("select id from test "))
  #      assert result == [(1,), (2,)]
         #self.assertTrue(myapp.store_integer(self.engine, 42))
-
+'''
 if __name__ == '__main__':
     if len(sys.argv) > 0:
         TestDB.DB_TEST_URL = sys.argv.pop()

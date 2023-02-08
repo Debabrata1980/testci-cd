614efd9ff531d88433fa1e357af92e9f0ccb61bb
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2023-02-08 18:12:49 +0530

    test

--- tests/db_test_upd.py
+++ tests/db_test_upd.py
@@ -24,7 +24,7 @@ class TestDB(unittest.TestCase):
    
         trans = self.connection.begin()
         self.connection.execute("CREATE TABLE accounts (age VARCHAR ( 50 ),name VARCHAR ( 50 ));")
-        self.connection.execute("INSERT INTO accounts (age,"name") values('22','deb')")
+        self.connection.execute("INSERT INTO accounts (age,name) values('22','deb')")
         trans.commit()
         result = list(self.connection.execute("select age from accounts "))
 #        assert result == [(1,), (22,)]
